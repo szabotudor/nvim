e1bd5e9b8a610ee62af6aa6997be13dbd2c5e5c1
szabotudor <szabo.tudor@gmail.com>
2025-10-17 01:52:33 +0200

    Add blink.cmp
    Plugin to complete using enables LSPs

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -10,4 +10,29 @@ return {
             vim.opt.termguicolors = true
         end,
     },
+
+    {
+        "saghen/blink.cmp",
+dependencies = { 'L3MON4D3/LuaSnip' },
+        version = "1.7",
+
+        opts = {
+            fuzzy = { implementation = "rust" },
+
+            sources = {
+                default = { 'lsp', 'path', 'snippets', 'buffer' },
+            },
+
+            snippets = { preset = "luasnip" },
+
+            keymap = {
+                ["<Tab>"] = {
+                    function (cmp)
+                        if cmp.is_visible() then return cmp.accept() end
+                    end,
+                    "fallback"
+                }
+            },
+        },
+    },
 }
