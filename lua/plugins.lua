6ace40c63bd2845db36cbb00205382828690e511
szabotudor <tudor.szabo@traverseresearch.nl>
2025-10-17 10:54:29 +0200

    Add treesitter and scope

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,5 +1,41 @@
 return {
     {
+        "nvim-treesitter/nvim-treesitter",
+
+        branch = "main",
+        lazy = false,
+        build = ":TSUpdate",
+
+        opts = {
+            indent = { enable = true },
+            ensure_installed = { "lua", "rust", "python", "cpp", "markdown" },
+            highlight = { enable = true },
+        },
+    },
+
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        main = "ibl",
+
+        opts = {
+            scope = {
+                enabled = true,
+                show_exact_scope = true,
+                show_end = false,
+                highlight = { "Function", "Label" },
+            },
+        },
+
+        config = function (_, opts)
+            local hooks = require("ibl.hooks")
+
+            hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
+
+            require("ibl").setup(opts)
+        end
+    },
+
+    {
         "nvim-tree/nvim-tree.lua",
         opts = {
             view = { width = 30 },
@@ -13,7 +49,7 @@ return {
 
     {
         "saghen/blink.cmp",
-dependencies = { 'L3MON4D3/LuaSnip' },
+        dependencies = { 'L3MON4D3/LuaSnip' },
         version = "1.7",
 
         opts = {
@@ -34,5 +70,26 @@ dependencies = { 'L3MON4D3/LuaSnip' },
                 }
             },
         },
+    },
+
+    {
+        "kdheepak/lazygit.nvim",
+        lazy = true,
+        cmd = {
+            "LazyGit",
+            "LazyGitConfig",
+            "LazyGitCurrentFile",
+            "LazyGitFilter",
+            "LazyGitFilterCurrentFile",
+        },
+        -- optional for floating window border decoration
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        -- setting the keybinding for LazyGit with 'keys' is recommended in
+        -- order to load the plugin when the command is run for the first time
+        keys = {
+            { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+        }
     },
 }
