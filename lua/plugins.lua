13cdb1169c4fe57760dca3d99a5759492515833c
szabotudor <szabo.tudor@gmail.com>
2025-10-23 23:05:24 +0200

    Remove dap-ui
    For now I will stick with a minimal config for debugging

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -123,12 +123,8 @@ return {
     },
 
     {
-        "rcarriga/nvim-dap-ui",
-
-        dependencies = {
-            "mfussenegger/nvim-dap",
-            "nvim-neotest/nvim-nio",
-        },
+        "mfussenegger/nvim-dap",
+        config = false,
     },
 
     {
