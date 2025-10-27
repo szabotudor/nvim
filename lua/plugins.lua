d6257aa6bb5b2b1f0b75d701f7e0581f31c0c3d8
szabotudor <tudor.szabo@traverseresearch.nl>
2025-10-27 12:07:31 +0100

    Install markdown preview plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,5 +1,15 @@
 return {
     {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        build = "cd app && yarn install",
+        init = function()
+            vim.g.mkdp_filetypes = { "markdown" }
+        end,
+        ft = { "markdown" },
+    },
+
+    {
         "nvim-tree/nvim-web-devicons",
 
         opts = {},
