9c45b2603df4d3e1603bb0f4023bed1033086746
szabotudor <tudor.szabo@traverseresearch.nl>
2025-12-03 17:13:13 +0100

    Configure rust analyzer to look for settings in the current dir

--- lua/lspcfg.lua
+++ lua/lspcfg.lua
@@ -59,7 +59,7 @@ end
 
 
 addlsp("lua_ls", "lua", true)
-addlsp("rust_analyzer", "rust")
+addlsp("rust_analyzer", "rust", true)
 addlsp("clangd", "c")
 addlsp("bashls", "bash", false)
 addlsp("pyright", "python")
