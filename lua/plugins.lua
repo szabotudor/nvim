66ba8a4d8e215319929052492b06e5c48b878aa2
szabotudor <tudor.szabo@traverseresearch.nl>
2026-04-23 11:57:49 +0200

    Add html

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -64,7 +64,7 @@ return {
 
         opts = {
             indent = { enable = true },
-            ensure_installed = { "lua", "rust", "python", "cpp", "markdown" },
+            ensure_installed = { "lua", "rust", "python", "cpp", "markdown", "html" },
             highlight = { enable = true },
         },
     },
