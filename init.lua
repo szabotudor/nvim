52668161cd4924d991c93652b9097cf3b9f53075
szabotudor <tudor.szabo@traverseresearch.nl>
2026-05-20 14:39:44 +0200

    Auto get new config versions when starting neovim

--- init.lua
+++ init.lua
@@ -1,3 +1,11 @@
+local config_dir = vim.fn.stdpath("config")
+local git_pull = vim.fn.system("git -C " .. config_dir .. " pull --ff-only 2>&1")
+if not git_pull:match("Already up to date") and not git_pull:match("error") and not git_pull:match("fatal") then
+    vim.notify("Neovim config updated, reloading...", vim.log.levels.INFO)
+    vim.cmd("source " .. config_dir .. "/init.lua")
+    return
+end
+
 vim.opt.termguicolors = true
 vim.g.nightflyCursorColor = true
 vim.g.nightflyNormalPmenu = true
